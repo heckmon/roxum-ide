e8b149e566f735f3f64b93526475eab3a2790ed6
ATHUL A S <64469847+heckmon@users.noreply.github.com>
2026-08-02 11:42:01 +0530

    2.3.2

--- lib/ui/about.dart
+++ lib/ui/about.dart
@@ -42,7 +42,7 @@ class AboutPage extends StatelessWidget {
               ),
               const SizedBox(height: 8),
               const Text(
-                'Version 2.3.0',
+                'Version 2.3.2',
                 style: TextStyle(fontSize: 16, color: Colors.grey),
               ),
               const SizedBox(height: 24),
